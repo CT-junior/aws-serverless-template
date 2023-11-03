4006eb9d751044cdb37ce2658de1759f1ee2d103
alekswheeler <alexskntks@outlook.com>
2023-11-03 11:42:43 -0300

    fix: hello world function

--- src/functions/hello/sayHello.ts
+++ src/functions/hello/sayHello.ts
@@ -1,16 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { Handler } from "src/errors/Handler";
-import RoomsRepositories from "src/repositories/implementations/RoomsRepositories";
 import { ok } from "src/utils/Returns";
 
 const sayHello = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
 
-  const dbteste = new RoomsRepositories();
-  await dbteste.findAll();
-
-  return ok("message", "Hello World!");
+  return ok("Hello world!", "message");
 };
 
 export const handler = Handler(sayHello);
