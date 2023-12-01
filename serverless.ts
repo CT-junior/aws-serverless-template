a73f92923bca30fc9d349125d08e0567160c87a9
alekswheeler <alexskntks@outlook.com>
2023-12-01 17:29:52 -0300

    Uodate serverless file

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 import { helloRoutes } from 'src/routes/hello.routes';
-import { quartosRoutes } from 'src/routes/quartos.routes';
+
+import { peopleRoutes } from './src/routes/people.routes';
 
 const serverlessConfiguration: AWS = {
   service: 'aws-serverless-template',
@@ -20,7 +21,7 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { ...helloRoutes, ...quartosRoutes },
+  functions: { ...helloRoutes, ...peopleRoutes },
   package: { individually: true },
   custom: {
     esbuild: {
